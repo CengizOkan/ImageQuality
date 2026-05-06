132bfd0be44966154c4c40a9ed1ed60fd6202146
Cengiz Okan <cengizokan@outlook.com>
2026-05-06 14:00:55 +0300

    yeni

--- src/utils/response.py
+++ src/utils/response.py
@@ -1,5 +1,5 @@
 from sdks.novavision.src.helper.package import PackageHelper
-from components.ImageQuality.src.models.PackageModel import (
+from capsules.ImageQuality.src.models.PackageModel import (
     PackageModel, PackageConfigs, ConfigExecutor,
     JpegQualityOutputs, JpegQualityResponse, JpegQuality,
     OutputImage
